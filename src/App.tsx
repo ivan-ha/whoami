f7f3042fa6a3a116c3e96903f4638db21b26d525
Ivan Ha <hangolam@gmail.com>
2018-12-22 10:20:11 +0800

    feat: replace with ReactFullpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverflow:true
+import ReactFullpage from "@fullpage/react-fullpage";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import "./styles.css";
+
+const fullpageOptions = {
+  anchors: ["firstPage", "secondPage", "thirdPage"],
+  sectionsColor: ["#282c34", "#ff5f45", "#0798ec"],
+  callbacks: ["onLeave", "afterLoad"],
+  scrollOverflow: true
+};
+
+const FullpageWrapper = fullpageProps => (
+  <ReactFullpage
+    {...fullpageProps}
+    render={({ state, fullpageApi }) => {
+      console.log("render prop change", state); // eslint-disable-line no-console
+
+      if (state.callback === "onLeave") {
+        if (state.direction === "down") {
+          console.log("going down..." + state.origin.index);
+        }
+      }
+      return (
+        <div id="fullpage-wrapper">
+          <div className="section section1">
+            <h3>Section 1</h3>
+            <button onClick={() => fullpageApi.moveSectionDown()}>
+              Move down
+            </button>
+          </div>
+          <div className="section">
+            <div className="slide">
+              <h3>Slide 2.1</h3>
+            </div>
+            <div className="slide">
+              <h3>Slide 2.2</h3>
+            </div>
+            <div className="slide">
+              <h3>Slide 2.3</h3>
+            </div>
+          </div>
+          <div className="section">
+            <h3>Section 3</h3>
+          </div>
+        </div>
+      );
+    }}
+  />
+);
+
+const App = () => <FullpageWrapper {...fullpageOptions} />;
 
 export default App;
