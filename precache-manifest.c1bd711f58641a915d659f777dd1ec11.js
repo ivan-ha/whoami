b884ada774c2538c15d840b71ca15cd19521c6be
Ivan Ha <hangolam@gmail.com>
2019-01-02 22:05:49 +0800

    Updates

---
+++ precache-manifest.c1bd711f58641a915d659f777dd1ec11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8891b2498bae6a40f06a",
+    "url": "/static/js/main.8891b249.chunk.js"
+  },
+  {
+    "revision": "7ebd5de18e4d7ecdc96f",
+    "url": "/static/js/1.7ebd5de1.chunk.js"
+  },
+  {
+    "revision": "8891b2498bae6a40f06a",
+    "url": "/static/css/main.26524fed.chunk.css"
+  },
+  {
+    "revision": "bfc9503e4b172b3096cb89cbec417888",
+    "url": "/index.html"
+  }
+];
